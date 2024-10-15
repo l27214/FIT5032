1b459f4d2c4e438e09df257f6ece4e761c968203
27214 <lsy27214@gmail.com>
2024-10-16 08:28:11 +1100

    Update the vite.config.js in sli-library

--- sli-library/vite.config.js
+++ sli-library/vite.config.js
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { build, defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
@@ -14,5 +14,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    outDir: 'dist',
   }
 })

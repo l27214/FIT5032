c4eaa6b4f4249b8e53bc53dba5a4b608e0213c5b
27214 <lsy27214@gmail.com>
2024-10-17 22:56:38 +1100

    Upadated config

--- sli-library/vite.config.js
+++ sli-library/vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: process.env.NODE_ENV === 'production' ? '/FIT5032/sli-library/' : '/',
+  base: '/',
   build: {
     outDir: 'dist',
   }
